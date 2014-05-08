1135826ff3e9cb75db5aff1805d202c9a1b3716e
Jeffrey Perrone <jperrone@well.com>
2014-05-07 18:44:23 -0700

    Set up environment variables.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -45,7 +45,7 @@ Rails.application.configure do
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
       :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
     }
-    :s3_host_name => 's3-us-west-1.amazonaws.com'
+    # :s3_host_name => 's3-us-west-1.amazonaws.com'
   }
 
 end
